f623f1f473f553a3dc6a63e99fcf6b48477c6bed
hilderonny <hilderonny2014@gmail.com>
2024-02-05 08:41:32 +0000

    Added color to filename column

--- public/scripts/main.mjs
+++ public/scripts/main.mjs
@@ -29,6 +29,16 @@ async function loadTasks() {
             const td = document.createElement("td")
             td.innerHTML = task[key]
             tr.appendChild(td)
+            if (key === "filename") {
+                var filename = task[key]
+                if (filename.length > 5) {
+                    var lastpart = filename.substring(filename.length - 6)
+                    var color = "#" + lastpart
+                    if (/^#[0-9A-F]{6}$/i.test(color)) {
+                        td.style.backgroundColor = color
+                    }
+                }
+            }
         }
         const actionTd = document.createElement("td")
         tr.appendChild(actionTd)
